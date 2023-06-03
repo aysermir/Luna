93f1669cda1034ea0d53a23f60f02c56d9b5049c
Mir Safwat Ayser <aysermir@gmail.com>
2023-06-03 01:00:16 -0400

    Added a template for the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import LoginButton from './components/login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="container">
+    <header className="header">
+      <div className="logo">My Social Media App</div>
+      <input type="text" placeholder="Search" />
+    </header>
+    <div className="content">
+      <div className="post">
+        <img src="image1.jpg" alt="Post 1" />
+        <p>This is the first post.</p>
+      </div>
+      <div className="post">
+        <img src="image2.jpg" alt="Post 2" />
+        <p>This is the second post.</p>
+      </div>
+      {/* Add more posts here */}
     </div>
+  </div>
   );
 }
 
---
+++ frontend/src/Apps.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LoginButton from './components/login';
+
+function Apps() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+
+    </div>
+  );
+}
+
+export default Apps;
