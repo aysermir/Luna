abcebfc45f4a60c8f01f8713bfe066aa6e078703
Mir Safwat Ayser <aysermir@gmail.com>
2023-06-15 01:19:37 -0400

    Created a palette and updated the nav to use the colours in the palette

---
+++ frontend/src/components/Palette.js
@@ -0,0 +1,15 @@
+const palette = {
+    colors: {
+      primary: "#007bff",
+      secondary: "#6c757d",
+      background: "#f8f9fa",
+      text: "#343a40",
+    },
+    fonts: {
+      heading: "'Roboto', sans-serif",
+      body: "'Open Sans', sans-serif",
+    },
+  };
+  
+  export default palette;
+  
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import palette from "./Palette";
 
 const Navbar = () => {
+  const { colors, fonts } = palette;
+
   return (
-    <nav className="navbar" style={{ backgroundColor: "lightgray", padding: "10px" }}>
+    <nav className="navbar" style={{ backgroundColor: colors.primary, padding: "1px" }}>
       <ul className="navbar-list" style={{ display: "flex", listStyleType: "none", justifyContent: "space-between" }}>
         <li className="navbar-item">
-        <span>Hi</span>
-        
+          <span>Home</span>
         </li>
         <li className="navbar-item">
-        <span>Hi</span>
-        
+        <span>Home</span>
         </li>
         <li className="navbar-item">
-         <span>Hi</span>
+        <span>Home</span>
         </li>
       </ul>
     </nav>
