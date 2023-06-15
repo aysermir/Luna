6fb3e6dba6aec242f966b197ddb8eab0afd660c9
Mir Safwat Ayser <aysermir@gmail.com>
2023-06-15 01:13:17 -0400

    Spaced out the elements in the nav

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,25 +3,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav
-      className="navbar"
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        backgroundColor: "lightgray",
-        padding: "10px"
-      }}
-    >
-      <ul className="navbar-list" style={{ display: "flex", listStyleType: "none" }}>
+    <nav className="navbar" style={{ backgroundColor: "lightgray", padding: "10px" }}>
+      <ul className="navbar-list" style={{ display: "flex", listStyleType: "none", justifyContent: "space-between" }}>
         <li className="navbar-item">
-          <span>Hi</span> 
+        <span>Hi</span>
+        
         </li>
         <li className="navbar-item">
-        <span>Hi</span> 
+        <span>Hi</span>
+        
         </li>
         <li className="navbar-item">
-        <span>Hi</span> 
+         <span>Hi</span>
         </li>
       </ul>
     </nav>
