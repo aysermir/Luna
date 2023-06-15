bc8a2b4165b6891ccf291d515ca041291333051c
Mir Safwat Ayser <aysermir@gmail.com>
2023-06-15 01:08:15 -0400

    Updated the navbar component

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar" style={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "lightgray", padding: "10px" }}>
+    <nav
+      className="navbar"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "lightgray",
+        padding: "10px"
+      }}
+    >
       <ul className="navbar-list" style={{ display: "flex", listStyleType: "none" }}>
-        <li className="navbar-item">Home</li>
-        <li className="navbar-item">About</li>
-        <li className="navbar-item">Contact</li>
+        <li className="navbar-item">
+          <span>Hi</span> 
+        </li>
+        <li className="navbar-item">
+        <span>Hi</span> 
+        </li>
+        <li className="navbar-item">
+        <span>Hi</span> 
+        </li>
       </ul>
     </nav>
   );
